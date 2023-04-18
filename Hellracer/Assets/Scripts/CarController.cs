64e5be48535562e169c0ab32babfd5cd6a171d24
Noah Glassford <56001967+noah-glassford@users.noreply.github.com>
2023-04-18 12:17:14 -0400

    Added FOV effect

--- Hellracer/Assets/Scripts/CarController.cs
+++ Hellracer/Assets/Scripts/CarController.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using Cinemachine;
 using TMPro;
+
 [System.Serializable]
 public class AxleInfo {
     public WheelCollider leftWheel;
@@ -17,11 +19,26 @@ public class CarController : MonoBehaviour {
     public float maxSteeringAngle;
     public ParticleSystem driftParticleLeft;
     public ParticleSystem driftParticleRight;
+
+    public ParticleSystem accelParticleLeft;
+    public ParticleSystem accelParticleRight;
     public TextMeshProUGUI SpeedDisplay;
     private CarInputActions inputActions;
     private PlayerInput playerInput;
     private float motorInput;
     private float steering;
+
+    [SerializeField]
+    CinemachineVirtualCamera VCam;
+    public bool ShouldAffectFOV;
+    public AnimationCurve CameraFOVCurve;
+    public float minSpeed = 0f;
+    public float maxSpeed = 25f;
+    private float playerSpeed;
+    private float FieldOfView;
+
+    public float sideDriftCutoff; //this and tirespin cutoff affects when smoke particles are spawned
+    public float tireSpinCutoff; 
 
     private void Start() 
     {
@@ -33,7 +50,9 @@ public class CarController : MonoBehaviour {
          playerInput = GetComponent<PlayerInput>();
          inputActions = new CarInputActions();
          inputActions.CarController.Enable();
-         inputActions.CarController.Accelerate.performed += Accelerate;
+
+        FieldOfView = 60;
+
     }
 
     // finds the corresponding visual wheel
@@ -55,20 +74,22 @@ public class CarController : MonoBehaviour {
         visualWheel.transform.position = position;
         visualWheel.transform.rotation = rotation;
     }
-
-    private void Accelerate(InputAction.CallbackContext obj)
-    {
-        motorInput = maxMotorTorque * obj.ReadValue<float>();
-    }
      
-    private void Update() 
-    {
-         steering = maxSteeringAngle * inputActions.CarController.Steering.ReadValue<float>();
-    }
     public void FixedUpdate()
     {
-       
-     
+        steering = maxSteeringAngle * inputActions.CarController.Steering.ReadValue<float>();
+        motorInput = maxMotorTorque * inputActions.CarController.Accelerate.ReadValue<float>();
+
+        if (ShouldAffectFOV)
+        {
+        playerSpeed = transform.GetChild(0).GetComponent<Rigidbody>().velocity.magnitude;
+      
+        float parameter = Mathf.InverseLerp(minSpeed, maxSpeed, playerSpeed);
+        Debug.Log(parameter);
+        parameter = CameraFOVCurve.Evaluate(parameter);
+        FieldOfView = Mathf.Lerp(60, 100, parameter);
+        VCam.m_Lens.FieldOfView = FieldOfView;
+        }
         foreach (AxleInfo axleInfo in axleInfos) 
         {
             if (axleInfo.steering) 
@@ -78,7 +99,6 @@ public class CarController : MonoBehaviour {
             }
             if (axleInfo.motor)
              {
-                Debug.Log(motorInput);
                 if (motorInput >= 0)
                 {
                     axleInfo.leftWheel.motorTorque = motorInput;
@@ -94,16 +114,15 @@ public class CarController : MonoBehaviour {
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
 
-            //SpeedDisplay.text = transform.GetChild(0).GetComponent<Rigidbody>().velocity.magnitude.ToString();
-
             WheelHit hit = new WheelHit();
-     
             WheelCollider leftWheel = axleInfo.leftWheel;
             WheelCollider rightWheel = axleInfo.rightWheel;
+
+
      
             if (leftWheel.GetGroundHit(out hit))
             {
-                if (hit.sidewaysSlip > .15 || hit.sidewaysSlip < -0.15)
+                if (hit.sidewaysSlip > sideDriftCutoff || hit.sidewaysSlip < -sideDriftCutoff)
                    driftParticleLeft.gameObject.SetActive(true);
                    else
                    driftParticleLeft.gameObject.SetActive(false);
@@ -111,10 +130,26 @@ public class CarController : MonoBehaviour {
 
             if (rightWheel.GetGroundHit(out hit))
             {
-                if (hit.sidewaysSlip > .15 || hit.sidewaysSlip < -0.15)
+                if (hit.sidewaysSlip > sideDriftCutoff || hit.sidewaysSlip < -sideDriftCutoff)
                    driftParticleRight.gameObject.SetActive(true);
                    else
                    driftParticleRight.gameObject.SetActive(false);
+            }
+
+             if (leftWheel.GetGroundHit(out hit))
+            {
+               if (hit.forwardSlip > tireSpinCutoff || hit.forwardSlip < -tireSpinCutoff)
+                   accelParticleLeft.gameObject.SetActive(true);
+                   else
+                   accelParticleLeft.gameObject.SetActive(false);
+            }
+
+            if (rightWheel.GetGroundHit(out hit))
+            {
+                if (hit.forwardSlip > tireSpinCutoff || hit.forwardSlip < -tireSpinCutoff)
+                   accelParticleRight.gameObject.SetActive(true);
+                   else
+                   accelParticleRight.gameObject.SetActive(false);
             }
         }
     }
